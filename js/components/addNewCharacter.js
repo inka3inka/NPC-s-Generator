469577c2ba2c00dd2093f189e42d5122f326ecda
inka3inka <inka3inka@gmail.com>
2019-11-17 11:31:56 +0100

    add New Character structure done

--- js/components/addNewCharacter.js
+++ js/components/addNewCharacter.js
@@ -1,10 +1,80 @@
 import React, {Component} from 'react';
+import elements from '../data/elements';
 
 export class NewCharacter extends Component {
+  state = {
+    characterName: "",
+    characterSurname: "",
+    attributesValue: [],
+    abilities: [],
+    armorClass: 0,
+    initiative: 0,
+    hitPoints: 0,
+    bio: ""
+  };
+
   render() {
     return (
-      <div>
-        New Character
+      <div className="new-character-container">
+        <div className="options">
+          <div className="attributes-container">
+            attributes
+            <label><span>strength</span>
+              <input type="number"/>
+            </label>
+            <label><span>dexterity</span>
+              <input type="number"/>
+            </label>
+            <label><span>constitution</span>
+              <input type="number"/>
+            </label>
+            <label><span>intelligence</span>
+              <input type="number"/>
+            </label>
+            <label><span>wisdom</span>
+              <input type="number"/>
+            </label>
+            <label><span>charisma</span>
+              <input type="number"/>
+            </label>
+
+          <form className="new-character-form">
+            <label><span>character's name</span>
+              <input type="text"/>
+            </label>
+            <label><span>character's surname</span>
+              <input type="text"/>
+            </label>
+            <label><span>race</span>
+              <select>
+                {elements.races.map(element =>
+                  <option key={element}>{element}</option>
+                )}
+              </select>
+            </label>
+            <label><span>class</span>
+              <select>
+                {elements.classes.map(element =>
+                  <option key={element}>{element}</option>
+                )}
+              </select>
+            </label>
+            <label><span>abilities</span>
+              <select className="abilities-to-select">
+                {elements.abilities.map(element =>
+                <option key={element.name}>{element.name}</option>
+                )}
+              </select>
+              <button>add</button>
+            </label>
+          </form>
+          </div>
+        </div>
+        <div className="chosen-options">
+          <ul className="chosen-abilities-container">
+            abilities
+          </ul>
+        </div>
       </div>
     )
   }
---
+++ js/data/elements.js
@@ -0,0 +1,79 @@
+export default
+  {
+    races: ["", "dwarf", "elf", "gnome", "half-elf", "halfling", "human"],
+    classes: ["", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "rogue", "sorcerer", "wizard"],
+    abilities: [
+      {
+        name: "acrobatics",
+        attributeConnected: "dexterity"
+      },
+      {
+        name: "animal handling",
+        attributeConnected: "wisdom"
+      },
+      {
+        name: "arcana",
+        attributeConnected: "intelligence"
+      },
+      {
+        name: "athletics",
+        attributeConnected: "strength"
+      },
+      {
+        name: "deception",
+        attributeConnected: "charisma"
+      },
+      {
+        name: "history",
+        attributeConnected: "intelligence"
+      },
+      {
+        name: "insight",
+        attributeConnected: "wisdom"
+      },
+      {
+        name: "intimidation",
+        attributeConnected: "charisma"
+      },
+      {
+        name: "investigation",
+        attributeConnected: "intelligence"
+      },
+      {
+        name: "medicine",
+        attributeConnected: "wisdom"
+      },
+      {
+        name: "nature",
+        attributeConnected: "intelligence"
+      },
+      {
+        name: "perception",
+        attributeConnected: "wisdom"
+      },
+      {
+        name: "performance",
+        attributeConnected: "charisma"
+      },
+      {
+        name: "persuasion",
+        attributeConnected: "charisma"
+      },
+      {
+        name: "religion",
+        attributeConnected: "intelligence"
+      },
+      {
+        name: "sleight of hand",
+        attributeConnected: "dexterity"
+      },
+      {
+        name: "stealth",
+        attributeConnected: "dexterity"
+      },
+      {
+        name: "survival",
+        attributeConnected: "wisdom"
+      }
+    ]
+  }
