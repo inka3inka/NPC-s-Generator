e0a453df377fc8a468d8b223e8ce37df167802ac
inka3inka <inka3inka@gmail.com>
2019-11-17 13:00:40 +0100

    Prepared handlers

--- js/components/addNewCharacter.js
+++ js/components/addNewCharacter.js
@@ -5,12 +5,74 @@ export class NewCharacter extends Component {
   state = {
     characterName: "",
     characterSurname: "",
-    attributesValue: [],
+    rece: "",
+    class: "",
+    attributesValue: {strength: "", dexterity: "", constitution: "", intelligence: "", wisdom: "", charisma: ""},
     abilities: [],
-    armorClass: 0,
-    initiative: 0,
-    hitPoints: 0,
+    armorClass: "",
+    initiative: "",
+    hitPoints: "",
     bio: ""
+  };
+
+  //Metody obsługujące inputy
+  handleNameChange = e => {
+    this.setState({
+      characterName: e.target.value
+    })
+  };
+
+  handleSurnameChange = e => {
+    this.setState({
+      characterSurname: e.target.value
+    })
+  };
+
+  handleRaceChange = e => {
+    this.setState({
+      race: e.target.value
+    })
+  };
+
+  handleClassChange = e => {
+    this.setState({
+      class: e.target.value
+    })
+  };
+
+  handleAttributeChange = e => {
+    const attributesValue = {...this.state.attributesValue};
+    attributesValue[e.target.name] = (e.target.value);
+    this.setState({
+      attributesValue: attributesValue
+    })
+  };
+
+  handleAbilitiesChosen = e => {
+    e.preventDefault();
+    const abilities = [...this.state.abilities];
+    abilities.push(this.selectEl.value);
+    this.setState({
+      abilities: abilities
+    }, () => console.log(this.state.abilities))
+  };
+
+  handleArmorClass = e => {
+    this.setState({
+      armorClass: e.target.value
+    })
+  };
+
+  handleInitiative = e => {
+    this.setState({
+      initiative: e.target.value
+    })
+  };
+
+  handleHitPoints = e => {
+    this.setState({
+      hitPoints: e.target.value
+    })
   };
 
   render() {
@@ -18,57 +80,169 @@ export class NewCharacter extends Component {
       <div className="new-character-container">
         <div className="options">
           <div className="attributes-container">
+
+      {/*Atrybuty*/}
+
             attributes
-            <label><span>strength</span>
-              <input type="number"/>
+            <label>
+              <span>
+                strength
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="strength"
+                value={this.state.attributesValue.strength}
+                onChange={this.handleAttributeChange}/>
             </label>
-            <label><span>dexterity</span>
-              <input type="number"/>
+            <label>
+              <span>
+                dexterity
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="dexterity"
+                value={this.state.attributesValue.dexterity}
+                onChange={this.handleAttributeChange}/>
             </label>
-            <label><span>constitution</span>
-              <input type="number"/>
+            <label>
+              <span>
+                constitution
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="constitution"
+                value={this.state.attributesValue.constitution}
+                onChange={this.handleAttributeChange}/>
             </label>
-            <label><span>intelligence</span>
-              <input type="number"/>
+            <label>
+              <span>
+                intelligence
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="intelligence"
+                value={this.state.attributesValue.intelligence}
+                onChange={this.handleAttributeChange}/>
             </label>
-            <label><span>wisdom</span>
-              <input type="number"/>
+            <label>
+              <span>
+                wisdom
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="attributesValue"
+                value={this.state.attributesValue[4]}
+                onChange={this.handleAttributeChange}/>
             </label>
-            <label><span>charisma</span>
-              <input type="number"/>
+            <label>
+              <span>
+                charisma
+              </span>
+              <input
+                className="attr"
+                type="number"
+                name="wisdom"
+                value={this.state.attributesValue.wisdom}
+                onChange={this.handleAttributeChange}/>
             </label>
 
+      {/* Formularz */}
+
           <form className="new-character-form">
-            <label><span>character's name</span>
-              <input type="text"/>
+
+          {/*inputy*/}
+            <label>
+              <span>
+                character's name
+              </span>
+              <input
+                type="text"
+                name="characterName"
+                value={this.state.characterName}
+                onChange={this.handleNameChange}/>
             </label>
-            <label><span>character's surname</span>
-              <input type="text"/>
+            <label>
+              <span>
+                character's surname
+              </span>
+              <input
+                type="text"
+                name="characterSurname"
+                value={this.state.characterSurname}
+                onChange={this.handleSurnameChange}/>
             </label>
-            <label><span>race</span>
-              <select>
-                {elements.races.map(element =>
-                  <option key={element}>{element}</option>
-                )}
+
+          {/*opcje*/}
+
+            <label>
+              <span>
+                race
+              </span>
+              <select
+                value={this.state.race}
+                onChange={this.handleRaceChange}>
+                {elements.races.map(element => <option key={element} value={element}>{element}</option>)}
               </select>
             </label>
-            <label><span>class</span>
-              <select>
-                {elements.classes.map(element =>
-                  <option key={element}>{element}</option>
-                )}
+            <label>
+              <span>
+                class
+              </span>
+              <select
+                value={this.state.class}
+                onChange={this.handleClassChange}>
+                {elements.classes.map(element => <option key={element} value={element}>{element}</option>)}
               </select>
             </label>
-            <label><span>abilities</span>
-              <select className="abilities-to-select">
-                {elements.abilities.map(element =>
-                <option key={element.name}>{element.name}</option>
-                )}
+            <label>
+              <span>
+                armor class
+              </span>
+              <input
+                type="number"
+                name="armorClass"
+                value={this.state.armorClass}
+                onChange={this.handleArmorClass}/>
+            </label>
+            <label>
+              <span>
+                initiative
+              </span>
+              <input
+                type="number"
+                name="initiative"
+                value={this.state.initiative}
+                onChange={this.handleInitiative}/>
+            </label>
+            <label>
+              <span>
+                hit points
+              </span>
+              <input
+                type="number"
+                name="hitPoints"
+                value={this.state.hitPoints}
+                onChange={this.handleHitPoints}/>
+            </label>
+            <label>
+              <span>
+                abilities
+              </span>
+              <select
+                className="abilities-to-select"
+                ref={(el) => this.selectEl = el}>
+                {elements.abilities.map(element => <option key={element.name} value={element.name} name={element.name}>{element.name}</option>)}
               </select>
-              <button>add</button>
+              <button onClick={this.handleAbilitiesChosen}>add</button>
             </label>
           </form>
           </div>
+          <div>Obrazek</div>
         </div>
         <div className="chosen-options">
           <ul className="chosen-abilities-container">
