bd20646930c0c59ad4dd9deafe13d6e837eff851
inka3inka <inka3inka@gmail.com>
2019-11-22 22:13:24 +0100

    characters sheet prepared

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,21 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000'
       }
-    ]
+    ],
+
   }
 };
