dffbe34521390d0c728b252b31f1f7cb16c71523
inka3inka <inka3inka@gmail.com>
2019-11-30 20:53:18 +0100

    component about filled, set change character's graphic

--- js/components/about.js
+++ js/components/about.js
@@ -3,8 +3,20 @@ import React, {Component} from 'react';
 export class About extends Component {
   render() {
     return (
-      <div>
-        About
+      <div className="main-container">
+        <div className="about-container">
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Pharetra sit amet aliquam id diam maecenas ultricies. Ipsum faucibus vitae aliquet nec ullamcorper. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Feugiat scelerisque varius morbi enim nunc. Euismod elementum nisi quis eleifend quam adipiscing. Id ornare arcu odio ut sem. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Augue ut lectus arcu bibendum at varius vel. Congue quisque egestas diam in arcu. Ut placerat orci nulla pellentesque. Libero justo laoreet sit amet cursus sit. Sollicitudin nibh sit amet commodo. Sed risus ultricies tristique nulla aliquet. Porta lorem mollis aliquam ut porttitor.
+          </p>
+
+          <p>
+          At tellus at urna condimentum. Tellus cras adipiscing enim eu turpis egestas pretium aenean. Egestas quis ipsum suspendisse ultrices gravida dictum. Pellentesque diam volutpat commodo sed. Amet facilisis magna etiam tempor orci eu lobortis. Posuere urna nec tincidunt praesent semper feugiat nibh. Nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi. Nascetur ridiculus mus mauris vitae. Sed elementum tempus egestas sed sed risus pretium quam vulputate. Dui vivamus arcu felis bibendum ut tristique et egestas quis. Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Lectus urna duis convallis convallis tellus id.
+          </p>
+
+          <p>
+          Enjoy!
+          </p>
+        </div>
       </div>
     )
   }
